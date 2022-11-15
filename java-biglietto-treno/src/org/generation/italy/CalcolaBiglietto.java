afc96a84b7a2abe0436de6b9fb06d03e1e517c43
Marc <102824384+EvoX7@users.noreply.github.com>
2022-11-15 15:49:45 +0100

    Java-exercise-1

---
+++ java-biglietto-treno/src/org/generation/italy/CalcolaBiglietto.java
@@ -0,0 +1,61 @@
+////
+////Ciao!
+////Esercitazione di oggi: Biglietto del treno
+////Nome repo: java-biglietto-treno
+////Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. 
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+////il prezzo del biglietto è definito in base ai km (0.21 € al km)
+////va applicato uno sconto del 20% per i minorenni
+////va applicato uno sconto del 40% per gli over 65
+////Come sempre create un progetto java con lo stesso nome del repo (java-biglietto-treno), 
+//	aggiungete un package org.generation.italy e una classe CalcolaBiglietto. Il programma va 
+//	implementato nel metodo main della classe CalcolaBiglietto.
+////Per acquisire l'input dell'utente usate la classe Scanner, come visto stamattina a lezione.
+////Buon lavoro!
+////
+////-------------------------------------
+////
+////BONUS1: i minori di 12 anni viaggiano gratis
+////BONUS2: ogni km costa 1 centesimo piu' del precedente (es: 1km = 0.21E, 2km = 0.43, 3km = 0.66E, ecc)
+
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+
+		System.out.print("Km che vuoi percorrere: ");
+		int km = sc.nextInt();
+
+		System.out.print("La tua eta': ");
+		int age = sc.nextInt();
+
+		sc.close();
+
+		float cost = (float) (km * .21f);
+
+		if (age <= 12) {
+			cost = 0;
+			System.out.printf("Il prezzo totale è: " + cost + "€");
+
+		} else if (age <= 18) {
+			cost = cost - (cost * 20) / 100;
+			System.out.printf("Il prezzo totale è: " + cost + "€");
+
+		} else if (age >= 65) {
+			cost = cost - (cost * 40) / 100;
+			System.out.printf("Il prezzo totale è: " + cost + "€");
+
+		} else {
+			System.out.printf("Il prezzo totale è: " + cost + "€");
+		}
+
+	}
+
+}
